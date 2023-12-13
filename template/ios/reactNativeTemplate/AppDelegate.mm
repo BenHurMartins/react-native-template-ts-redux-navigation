68df5231a5e3088581a69f8b67fc2355e2e4a885
Ben Hur <benhur.martins@hotmail.com>
2023-12-13 11:51:53 -0300

    fix: ios issues

--- template/ios/reactNativeTemplate/AppDelegate.mm
+++ template/ios/reactNativeTemplate/AppDelegate.mm
@@ -11,8 +11,9 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
- [RNSplashScreen show]; 
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+  [super application:application didFinishLaunchingWithOptions:launchOptions];
+  [RNSplashScreen show]; // here
+  return YES;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
